675e08427c0ab8edca9dd480b9caabf10cacbc71
Nícolas <nicolasdamotaarruda@hotmail.com>
2025-07-22 20:46:19 -0300

    feat: Adicionando sistema de transporte após diálogo

--- Assets/PlayerCanMove.cs
+++ Assets/PlayerCanMove.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class PlayerCanMove : MonoBehaviour
 {
     public PlayerController Player;
+    public GameObject porta;
+
     void Start()
     {
-        Player.canMove = true;
+        //Player.canMove = true;
+        porta.SendMessage("interacao", false, SendMessageOptions.DontRequireReceiver);   //Inicia a transição para a casa do pai
     }
 }
