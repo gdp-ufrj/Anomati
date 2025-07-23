747c77911bd3316536bc880cdff6ae049e5d81e1
Nícolas <nicolasdamotaarruda@hotmail.com>
2025-07-22 21:17:25 -0300

    fix: Consertando bug da câmera

--- Assets/PlayerCanMove.cs
+++ Assets/PlayerCanMove.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerCanMove : MonoBehaviour
 {
     public PlayerController Player;
     public GameObject porta;
+    public bool isTeleport = false;
 
     void Start()
     {
-        //Player.canMove = true;
+        if (isTeleport)
+        {
+            StartCoroutine(Teleport());
+        }
+        else
+        {
+            Player.canMove = true;
+        }
+    }
+
+    private IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(1f);   //Esperando 1 srgundo antes de iniciar a transição
         porta.SendMessage("interacao", false, SendMessageOptions.DontRequireReceiver);   //Inicia a transição para a casa do pai
     }
 }
