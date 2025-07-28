37e04c65e8dfab1604fe56f9716bf2bc4daf15de
BrunoTzki <brunocamenietzki@eefd.ufrj.br>
2025-07-28 16:24:38 -0300

    Montagem de Sons até o final

--- Assets/GatilhoDeTimeline.cs
+++ Assets/GatilhoDeTimeline.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class GatilhoDeTimeline : MonoBehaviour
 {
     public GameObject TimelineObject;
+    public bool BlockMovement = true;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("player"))
         {
             TimelineObject.SetActive(true);
-            GameControllerNicolas.GetInstance().DisablePlayerMovement();
+            if (BlockMovement)
+            {
+                GameControllerNicolas.GetInstance().DisablePlayerMovement();
+            }
         }
     }
 
